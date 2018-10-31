33e895da7e85f40fbc5a2c09b8f9eb0abe7fc716
renjiroazhar <renjiroazhar@gmail.com>
2018-11-01 02:08:43 +0700

    Account Page unfixed

--- web/src/Containers/Account/AccountContainer.jsx
+++ web/src/Containers/Account/AccountContainer.jsx
@@ -3,15 +3,19 @@ import {
   withStyles
 } from "@material-ui/core/styles";
 import PropTypes from "prop-types";
-
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
+import { Link } from 'react-router-dom';
 
 const styles = {
   container: {
     width: '100%',
-    textAlign : "center",
-    backgroundColor : "blue",
-    height: "100%"
-  }
+    textAlign: "left",
+    backgroundColor: "#fff",
+    height: "100%",
+    margin: "15px",
+    padding: "15px"
+  },
 };
 
 
@@ -19,8 +23,33 @@ class AccountContainer extends Component {
   render() {
     const { classes } = this.props;
     return (
-      <div className={classes.container}>
-          <p>Account</p>
+      <div>
+        <div className={classes.container}>
+          <div style={{ textAlign: 'right' }}>
+            <Button variant="contained" color="primary" aria-label="Add">
+              Edit <AddIcon />
+            </Button>
+          </div>
+          <h5>Username : </h5>
+          <h5>Email : </h5>
+          <h5>Phone : </h5>
+        </div>
+
+        <div className={classes.container}>
+          <h5>FAQ <AddIcon /></h5>
+        </div>
+
+        <div className={classes.container}>
+          <h5>Rate Gambar</h5>
+        </div>
+
+        <div style={{ textAlign: 'center', marginTop: '20px' }}>
+          <Link to="/" style={{ textDecoration: 'none' }}>
+            <Button variant="contained" color="secondary" style={{ height: '20px', width: '150px' }}>
+              Logout
+            </Button>
+          </Link>
+        </div>
       </div>
     )
   }
