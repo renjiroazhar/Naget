75980be9f1ea00aef9ceb3d68b359dec796e43fa
renjiroazhar <renjiroazhar@gmail.com>
2018-11-07 21:55:57 +0700

    sticky-appbar

--- web/src/Components/Header.jsx
+++ web/src/Components/Header.jsx
@@ -8,31 +8,35 @@ const styles = {
   root: {
     flexGrow: 1,
     position: "fixed", /* Safari */
-    width : "100%",
+    width: "100%",
     top: 0,
     zIndex: 100,
   },
 };
 
 class Header extends Component {
-  
+
   render() {
     const { classes } = this.props;
     return (
       <div>
         <div className={classes.root}>
-      <AppBar position="static" color="default" style={{ backgroundColor : "#559351"}}>
-        <Toolbar>
-        <div style={{display: "block",
-    margin: "0 auto"}} >
-          <img src="https://www.moretrash.id/wp-content/uploads/2018/05/logo-moretrash.png" srcset="https://www.moretrash.id/wp-content/uploads/2018/05/logo-moretrash.png 1x" width="171" height="50" alt="Moretrash Logo" retina_logo_url="" className="fusion-standard-logo" style={{display: "block",
-    margin: "0 auto", padding: "12px"}}   />
-          </div>
-        </Toolbar>
-      </AppBar>
-    </div>
-    <br/>
-    <br/> 
+          <AppBar position="static" color="default" style={{ backgroundColor: "#559351" }}>
+            <Toolbar>
+              <div style={{
+                display: "block",
+                margin: "0 auto"
+              }} >
+                <img src="https://www.moretrash.id/wp-content/uploads/2018/05/logo-moretrash.png" srcset="https://www.moretrash.id/wp-content/uploads/2018/05/logo-moretrash.png 1x" width="171" height="50" alt="Moretrash Logo" retina_logo_url="" className="fusion-standard-logo" style={{
+                  display: "block",
+                  margin: "0 auto", padding: "12px"
+                }} />
+              </div>
+            </Toolbar>
+          </AppBar>
+        </div>
+        <br />
+        <br />
       </div>
     )
   }
