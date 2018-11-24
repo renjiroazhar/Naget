59e7e899966470abdbf9ed6100af85cc1e3e5cc8
renjiroazhar <41312213+renjiroazhar@users.noreply.github.com>
2018-11-24 15:47:52 +0700

    Revert "fix margin"

--- web/src/Containers/Dashboard/StepLogin/Review.jsx
+++ web/src/Containers/Dashboard/StepLogin/Review.jsx
@@ -5,7 +5,7 @@ import Typography from '@material-ui/core/Typography';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
-
+import Grid from '@material-ui/core/Grid';
 
 const products = [
   { name: 'Product 1', desc: 'A nice thing', price: '$9.99' },
@@ -13,6 +13,13 @@ const products = [
   { name: 'Product 3', desc: 'Something else', price: '$6.51' },
   { name: 'Product 4', desc: 'Best thing of all', price: '$14.11' },
   { name: 'Shipping', desc: '', price: 'Free' },
+];
+const addresses = ['1 Material-UI Drive', 'Reactville', 'Anytown', '99999', 'USA'];
+const payments = [
+  { name: 'Card type', detail: 'Visa' },
+  { name: 'Card holder', detail: 'Mr John Smith' },
+  { name: 'Card number', detail: 'xxxx-xxxx-xxxx-1234' },
+  { name: 'Expiry date', detail: '04/2024' },
 ];
 
 const styles = theme => ({
@@ -48,7 +55,32 @@ function Review(props) {
           </Typography>
         </ListItem>
       </List>
-
+      <Grid container spacing={16}>
+        <Grid item xs={12} sm={6}>
+          <Typography variant="h6" gutterBottom className={classes.title}>
+            Shipping
+          </Typography>
+          <Typography gutterBottom>John Smith</Typography>
+          <Typography gutterBottom>{addresses.join(', ')}</Typography>
+        </Grid>
+        <Grid item container direction="column" xs={12} sm={6}>
+          <Typography variant="h6" gutterBottom className={classes.title}>
+            Payment details
+          </Typography>
+          <Grid container>
+            {payments.map(payment => (
+              <React.Fragment key={payment.name}>
+                <Grid item xs={6}>
+                  <Typography gutterBottom>{payment.name}</Typography>
+                </Grid>
+                <Grid item xs={6}>
+                  <Typography gutterBottom>{payment.detail}</Typography>
+                </Grid>
+              </React.Fragment>
+            ))}
+          </Grid>
+        </Grid>
+      </Grid>
     </React.Fragment>
   );
 }
