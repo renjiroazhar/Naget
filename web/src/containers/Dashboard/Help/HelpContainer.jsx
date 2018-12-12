d873e8113cfa402585ecf4d1df4d62d4b0f17d0f
renjiroazhar <renjiroazhar@gmail.com>
2018-12-13 04:57:29 +0700

    Help Page

--- web/src/containers/Dashboard/Help/HelpContainer.jsx
+++ web/src/containers/Dashboard/Help/HelpContainer.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import Navbar from '../../../components/Navbar';
 import { Accordion } from 'antd-mobile';
-import Help from './svg/Help.svg';
 
 const myAccordion = {
     padding: "15px",
@@ -16,13 +15,7 @@ export default class HelpContainer extends Component {
             <div>
                 <Navbar style={{ position: 'content' }} />
 
-                <div style={{ width: "90px", height: "90px", }} >
-                    <center>
-                        <Help />
-                    </center>
-                </div>
-
-                <div style={{ marginTop: "10px", }} >
+                <div style={{ marginTop: "80px", marginBottom: "10px", }} >
                     <Accordion defaultActiveKey="0" className={myAccordion} onChange={this.onChange}>
                         <Accordion.Panel header="Apa Moretrash itu?" className={myAccordion}>
                             text text text text text text text text text text text text text text text
