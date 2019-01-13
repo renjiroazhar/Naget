88458dcb177a6242fd54a352db5c34882ce9f785
Arif Widianto <arifjozz123@gmail.com>
2019-01-13 20:46:20 +0700

    Add Admin

--- web/src/index.js
+++ web/src/index.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import 'antd-mobile/dist/antd-mobile.css';
-import App from './App';
-import * as serviceWorker from './service-worker';
-import rootReducers from './redux/reducers/rootReducers';
-import { createBrowserHistory } from 'history';
-import { createStore, applyMiddleware, compose } from 'redux';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import { reduxFirestore, getFirestore } from 'redux-firestore';
-import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
-import firebaseConfig from './services/firebaseConfig';
-import { Router } from 'react-router-dom';
-import AuthProvider from './context/AuthProvider';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "antd-mobile/dist/antd-mobile.css";
+import App from "./App";
+import * as serviceWorker from "./service-worker";
+import rootReducers from "./redux/reducers/rootReducers";
+import { createBrowserHistory } from "history";
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { reduxFirestore, getFirestore } from "redux-firestore";
+import { reactReduxFirebase, getFirebase } from "react-redux-firebase";
+import firebaseConfig from "./services/firebaseConfig";
+import { Router } from "react-router-dom";
+import AuthProvider from "./context/AuthProvider";
 
 const store = createStore(
   rootReducers,
@@ -22,13 +22,13 @@ const store = createStore(
     reduxFirestore(firebaseConfig),
     reactReduxFirebase(firebaseConfig, {
       useFirestoreForProfile: true,
-      userProfile: 'users',
+      userProfile: "users",
       attachAuthIsReady: true
     })
   )
 );
 
-const history = createBrowserHistory();
+const browserHistory = createBrowserHistory();
 
 const Main = () => {
   return (
@@ -41,11 +41,11 @@ const Main = () => {
 store.firebaseAuthIsReady.then(() => {
   ReactDOM.render(
     <Provider store={store}>
-      <Router history={history}>
+      <Router history={browserHistory}>
         <Main />
       </Router>
     </Provider>,
-    document.getElementById('root')
+    document.getElementById("root")
   );
   serviceWorker.unregister();
   if (module.hot) {
