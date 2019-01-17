cc692c849d9858df4785680d95cd817a8b8ccfb0
renjiroazhar <renjiroazhar@gmail.com>
2019-01-17 20:17:18 +0700

    app.js

--- web/src/App.js
+++ web/src/App.js
@@ -7,35 +7,19 @@ import { AuthContext } from './containers/mobile/context/AuthProvider';
 import { cssInJs } from './assets/style/splashScreen';
 
 class App extends Component {
-<<<<<<< HEAD
   render() {
     const { loading, isAuthenticated } = this.context.state;
     if (loading) {
       return (
         <div style={cssInJs.backgroundLoading}>
           <div style={cssInJs.loading}>
-            <img src={logo} alt="splash-screen" width="400" height="200" />{' '}
+            <img src={logo} alt="splash-screen" width="175" height="45" />{' '}
           </div>
         </div>
       );
     }
     return isAuthenticated ? <PrivateRoute /> : <PublicRoute />;
   }
-=======
-	render() {
-		const { loading, isAuthenticated } = this.context.state;
-		if (loading) {
-			return (
-				<div style={cssInJs.backgroundLoading}>
-					<div style={cssInJs.loading}>
-						<img src={logo} alt="splash-screen" width="175" height="45" />{' '}
-					</div>
-				</div>
-			);
-		}
-		return isAuthenticated ? <PrivateRoute /> : <PublicRoute />;
-	}
->>>>>>> parent of 7f1394c... Fix Selected (Karena sempet ilang)
 }
 
 App.contextType = AuthContext;
